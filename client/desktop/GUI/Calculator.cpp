f1825bd7c86272773d655aec4d5106556ad8cd60
Bohdan Volkov <bohdan.volkov@dxc.com>
2023-09-03 20:09:44 +0300

    update calculator

--- client/desktop/GUI/Calculator.cpp
+++ client/desktop/GUI/Calculator.cpp
@@ -2,14 +2,146 @@
 #include "ui_calculator.h"
 
 Calculator::Calculator(QWidget *parent) : QWidget(parent),
-                                          ui(new Ui::calculator)
+                                          mUi(new Ui::calculator)
 {
-    ui->setupUi(this);
+    mUi->setupUi(this);
     this->setWindowTitle("Калькулятор");
     this->hide();
+
+    if (mUi != NULL)
+    {
+        mUi->calcTableWidget->setRowCount(1);
+        mUi->calcTableWidget->setColumnCount(4);
+
+        mUi->calcTableWidget->setHorizontalHeaderLabels({"Название", "Кол-во", "Един. измерения", "Цена за кг."});
+
+        QHeaderView *header = mUi->calcTableWidget->horizontalHeader();
+
+        // Устанавливаем режим растяжения для каждой колонки
+        for (int col = 0; col < mUi->calcTableWidget->columnCount(); ++col)
+        {
+            header->setSectionResizeMode(col, QHeaderView::Stretch);
+        }
+
+        QComboBox *comboBox = createComboBoxWithUnity();
+
+        mUi->calcTableWidget->setCellWidget(0, 2, comboBox);
+
+        mUi->calcTableWidget->setEditTriggers(QAbstractItemView::DoubleClicked);
+
+        connect(mUi->addIngridient, SIGNAL(clicked()), this, SLOT(addNewIngridient()));
+        connect(mUi->calcCost, SIGNAL(clicked()), this, SLOT(calcCost()));
+        connect(mUi->calcTableWidget, SIGNAL(clicked(const QModelIndex &index)), this, SLOT(removeIngridient(const QModelIndex &index)));
+    }
 }
 
 Calculator::~Calculator()
 {
-    delete ui;
+    delete mUi;
+}
+
+void Calculator::addNewIngridient()
+{
+    int rowCount = mUi->calcTableWidget->rowCount(); // Получаем текущее количество строк
+
+    QComboBox *comboBox = createComboBoxWithUnity();
+
+    mUi->calcTableWidget->insertRow(rowCount); // Добавляем новую строку
+    mUi->calcTableWidget->setCellWidget(rowCount, 2, comboBox);
+}
+
+void Calculator::copyLayout(QLayout *sourceLayout, QLayout *targetLayout)
+{
+    for (int i = 0; i < sourceLayout->count(); ++i)
+    {
+        QLayoutItem *item = sourceLayout->itemAt(i);
+        if (item)
+        {
+            QWidget *widget = item->widget();
+            if (widget)
+            {
+                targetLayout->addWidget(widget);
+            }
+        }
+    }
+}
+
+void Calculator::calcCost()
+{
+    int rowCount = mUi->calcTableWidget->rowCount();
+    int colCount = mUi->calcTableWidget->columnCount();
+    double productCost = 0;
+
+    QVector<Ingredient> tableData; // Вектор для хранения данных
+
+    for (int row = 0; row < rowCount; ++row)
+    {
+        Ingredient rowData; // Вектор для хранения данных строки
+        for (int col = 0; col < colCount; ++col)
+        {
+            QTableWidgetItem *item = mUi->calcTableWidget->item(row, col);
+            if (item)
+            {
+                QString cellData = "";
+
+                QComboBox *comboBox = qobject_cast<QComboBox *>(mUi->calcTableWidget->cellWidget(row, col));
+                if (comboBox)
+                {
+                    cellData = comboBox->currentText();
+                }
+                else
+                {
+                    cellData = item->text();
+                }
+
+                switch (col)
+                {
+                case 0:
+                    rowData.name = cellData;
+                    break;
+                case 1:
+                    rowData.quantity = cellData;
+
+                    break;
+                case 1:
+                    rowData.unity = cellData;
+
+                    break;
+                case 1:
+                    rowData.price = cellData;
+
+                    break;
+                default:
+                    break;
+                }
+            }
+        }
+        tableData.push_back(rowData);
+    }
+
+    for (int i = 0; i < tableData.size(); i++)
+    {
+        productCost += (tableData[i][1].toInt() * (tableData[i][3].toInt()) / 1000);
+    }
+
+    mUi->productCost->setText(QString::number(productCost) + " грн.");
+}
+
+void Calculator::removeIngridient(const QModelIndex &index)
+{
+    if (index.column() == 0)
+    {
+        mUi->calcTableWidget->removeRow(index.row());
+    }
+}
+
+QComboBox *Calculator::createComboBoxWithUnity()
+{
+    QComboBox *comboBox = new QComboBox;
+    comboBox->addItem("кг");
+    comboBox->addItem("г");
+    comboBox->addItem("л");
+    comboBox->addItem("мл");
+
+    return comboBox;
 }
--- client/desktop/GUI/Calculator.h
+++ client/desktop/GUI/Calculator.h
@@ -2,11 +2,20 @@
 #define CALCULATOR_H
 
 #include <QWidget>
+#include <QComboBox>
 
 namespace Ui
 {
     class calculator;
 }
+
+struct Ingredient
+{
+    QString name;
+    QString quantity;
+    QString unity;
+    QString price;
+};
 
 class Calculator : public QWidget
 {
@@ -16,8 +25,15 @@ public:
     explicit Calculator(QWidget *parent = 0);
     ~Calculator();
 
+public slots:
+    void addNewIngridient();
+    void copyLayout(QLayout *sourceLayout, QLayout *targetLayout);
+    void calcCost();
+    void removeIngridient(const QModelIndex &index);
+    QComboBox *createComboBoxWithUnity();
+
 private:
-    Ui::calculator *ui;
+    Ui::calculator *mUi;
 };
 
 #endif // CALCULATOR_H
